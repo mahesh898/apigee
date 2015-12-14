2fe0411add1488934bd21b6c1a2a7668240bf3f6
dzuluaga <dzuluaga@apigee.com>
2015-12-13 17:54:48 -0800

    change README

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 mvn install -P$env -Dusername=$ae_username -Dpassword=$ae_password -Dorg=$org
 echo "Test your APIs with the following http commands"
 echo "Check this URL to test check San Francisco weather: https://$org-$env.apigee.net/weather/forecastrss?w=2487956"
-echo "To check for directions http://localhost:3000/builds/$UUID/src/gateway/forecastweatherapi/directions.html"
+echo "To check for directions https://ec2-52-23-232-127.compute-1.amazonaws.com/builds/$UUID/directions.html"
 echo "Test your APIs with the following http commands" >> directions.html
 echo "Check this URL to test check San Francisco weather: https://$org-$env.apigee.net/weather/forecastrss?w=2487956" >> directions.html
