2a55b30a1905449ccd7b111cc96f9d0273a174f9
dzuluaga <dzuluaga@apigee.com>
2015-12-13 17:10:26 -0800

    adding instructions

--- src/gateway/forecastweatherapi/makeScript.sh
+++ src/gateway/forecastweatherapi/makeScript.sh
@@ -1,1 +1,4 @@
 mvn install -P$env -Dusername=$ae_username -Dpassword=$ae_password -Dorg=$org
+echo "Test your APIs with the following http commands"
+touch directions.html
+echo "Test your APIs with the following http commands" > directions.html
