56ccf4d5ddf047d2e2b5a0ec089c55d872a9658f
dzuluaga <dzuluaga@apigee.com>
2015-12-13 17:23:27 -0800

    provide links

--- src/gateway/forecastweatherapi/makeScript.sh
+++ src/gateway/forecastweatherapi/makeScript.sh
@@ -1,4 +1,4 @@
 mvn install -P$env -Dusername=$ae_username -Dpassword=$ae_password -Dorg=$org
 echo "Test your APIs with the following http commands"
-touch directions.html
+echo "Check this URL to test check San Francisco weather: https://$org-$env.apigee.net/weather/forecastrss?w=2487956"
 echo "Test your APIs with the following http commands" > directions.html
