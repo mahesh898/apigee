fe097a6115250570bb167ef5dc899ab6e5670c1a
dzuluaga <dzuluaga@apigee.com>
2015-12-13 17:31:48 -0800

    path to directions

--- src/gateway/forecastweatherapi/makeScript.sh
+++ src/gateway/forecastweatherapi/makeScript.sh
@@ -1,4 +1,6 @@
 mvn install -P$env -Dusername=$ae_username -Dpassword=$ae_password -Dorg=$org
 echo "Test your APIs with the following http commands"
 echo "Check this URL to test check San Francisco weather: https://$org-$env.apigee.net/weather/forecastrss?w=2487956"
-echo "Test your APIs with the following http commands" > directions.html
+echo "To check for directions http://localhost:3000/build/$UUID/src/gateway/forecastweatherapi/directions.html"
+echo "Test your APIs with the following http commands" >> directions.html
+echo "Check this URL to test check San Francisco weather: https://$org-$env.apigee.net/weather/forecastrss?w=2487956" >> directions.html
